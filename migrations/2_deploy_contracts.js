1392cf629f4cfb2ef0fd62b64676a0955b723014
Wayne <waynedobson@usa.net>
2019-11-06 15:46:16 +0700

    Remove hard coded addresses in deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,9 @@
 var Splitter = artifacts.require("./Splitter.sol");
 
-const aliceAddress = "0x695E0E26BAe141f014921c12C92D45fF845bf32c";
-const bobAddress = "0xE75e73F417930F23eAd63180A05E132aF97b39e5";
-const carolAddress = "0xb1885953430884544Bc205A90A6e2c810d433013";
+module.exports = function(deployer, network, accounts) {
+  const aliceAddress = accounts[0];
+  const bobAddress = accounts[1];
+  const carolAddress = accounts[2];
 
-module.exports = function(deployer) {
   deployer.deploy(Splitter, aliceAddress, bobAddress, carolAddress);
 };
